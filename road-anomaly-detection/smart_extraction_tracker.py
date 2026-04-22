ed948b88b0aefaed48b7a8415bf7bf647be88583
bberkedemir <asdafdasx1@gmail.com>
2026-04-22 18:02:56 +0300

    Now holds the track IDs in a dictionary with their confidence data. Overwrites the image if the new frame has better confidence.

--- road-anomaly-detection/smart_extraction_tracker.py
+++ road-anomaly-detection/smart_extraction_tracker.py
@@ -11,22 +11,23 @@ SAVE_SIDE_BY_SIDE_TEST = True
 model = YOLO("best.pt")
 
 # Folders
-VID_DIR = Path("test_videos/videos")
-OUTPUT_DIR = VID_DIR / "detected_output"
-EXTRACT_DIR = VID_DIR / "extracted_anomalies"
-TEST_DIR = VID_DIR / ".." / "testing_side_by_side"
+TEST_DIR = Path("test_videos")
+VID_DIR = TEST_DIR / "videos"
+OUTPUT_DIR = TEST_DIR / "detected_output"
+EXTRACT_DIR = TEST_DIR / "extracted_anomalies"
+COMPARE_DIR = TEST_DIR / "testing_side_by_side"
 
 OUTPUT_DIR.mkdir(parents=True, exist_ok=True)
 EXTRACT_DIR.mkdir(parents=True, exist_ok=True)
 if SAVE_SIDE_BY_SIDE_TEST:
-    TEST_DIR.mkdir(parents=True, exist_ok=True)
+    COMPARE_DIR.mkdir(parents=True, exist_ok=True)
 
 # Get Videos
 videos = glob.glob(f"{VID_DIR}/*.mp4")
 
-# keeps track of saved track IDs, we include video names in case we restart the tracking per video.
-# tuple (vid_name, track_id) ensures uniqueness across different videos
-saved_anomalies = set()
+# Dictionary to hold the highest confidence data for anomalies.
+# Key: (vid_name, track_id), Value: { 'conf': float, 'class_name': str, 'timestamp': float, 'path': str }
+best_anomalies = {}
 
 for video in videos:
     capture = cv2.VideoCapture(video)
@@ -47,6 +48,9 @@ for video in videos:
         if not ret:
             break
         
+        # get timestamp of frame
+        timestamp = capture.get(cv2.CAP_PROP_POS_MSEC) / 1000
+
         # Enable tracking with ByteTrack
         results = model.track(frame, persist=True, tracker="bytetrack.yaml", conf=0.5, verbose=False)
         
@@ -61,12 +65,14 @@ for video in videos:
                 if box.id is not None:
                     track_id = int(box.id[0])
                     class_id = int(box.cls[0])
+                    conf = float(box.conf[0])
                     class_name = model.names[class_id]
                     
                     # Create a unique key for the anomaly
                     anomaly_key = (vid_name, track_id)
                     
-                    if anomaly_key not in saved_anomalies:
+                    # New or better frame
+                    if anomaly_key not in best_anomalies or conf > best_anomalies[anomaly_key]['conf']:
                         x1, y1, x2, y2 = map(int, box.xyxy[0])
                         
                         h, w, _ = frame.shape
@@ -79,14 +85,26 @@ for video in videos:
                         if crop.size > 0:
                             img_name = f"{vid_name}_{class_name}_id_{track_id}.jpg".replace(" ", "_")
                             img_path = EXTRACT_DIR / img_name
-                            cv2.imwrite(str(img_path), crop)
-                            print(f"Extracted {class_name}: {img_name}")
-                            
-                            # Cache the key so we never save this specific tracked anomaly again
-                            saved_anomalies.add(anomaly_key)
 
+                            if anomaly_key not in best_anomalies:
+                                print(f"Extracted {class_name}: {img_name}")
+                            elif conf > best_anomalies[anomaly_key]['conf']:
+                                print(f"Overwriting {class_name}: {img_name}. Old conf: {round(best_anomalies[anomaly_key]['conf'], 3)}, New conf: {round(conf, 3)}")
+
+                            # overwrite if better conf
+                            cv2.imwrite(str(img_path), crop)
+
+                            # Update best record
+                            best_anomalies[anomaly_key] = {
+                                'conf': conf,
+                                'class_name': class_name,
+                                'timestamp': timestamp,
+                                'path': str(img_path)
+                            }
+
+                            
                             if SAVE_SIDE_BY_SIDE_TEST:
-                                test_img_path = TEST_DIR / img_name
+                                test_img_path = COMPARE_DIR / img_name
                                 # scale both images
                                 target_h = 720
 
@@ -105,7 +123,6 @@ for video in videos:
                                 cv2.putText(side_by_side, f"ID: {track_id}", (20, 50), cv2.FONT_HERSHEY_SIMPLEX, 1.2, (0, 0, 255), 3)
                                 cv2.imwrite(str(test_img_path), side_by_side)
 
-                                print(f"Registered {class_name} [{track_id}] in {test_img_path}")
 
 
             
@@ -118,4 +135,4 @@ for video in videos:
 print(f"All tracking finished. Check {EXTRACT_DIR} for extracted anomalies.")
 
 if SAVE_SIDE_BY_SIDE_TEST:
-    print(f"Side by side tests stored in: {TEST_DIR}")
+    print(f"Side by side tests stored in: {COMPARE_DIR}")
