8aa3a81913cedab4d442d5d1fa67cd11a1e2acf0
Yavuz Selim <yavuzgezginci24@gmail.com>
2026-05-07 18:25:02 +0300

    ip change

--- smart-waste-mobile/services/api.ts
+++ smart-waste-mobile/services/api.ts
@@ -16,7 +16,7 @@ import type {
 } from '../types';
 
 // Use LAN IP (not localhost) when testing on a physical device via Expo Go
-export const API_BASE_URL = 'http://10.105.126.189:8000';
+export const API_BASE_URL = 'http://10.93.122.89:8000';
 
 //export const API_BASE_URL = 'https://arise-deprive-disobey.ngrok-free.dev';
 
